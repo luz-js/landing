c288aa039f56bddf7904d7c0aa6a9d336c2ab2f1
luz-js <68757372+luz-js@users.noreply.github.com>
2021-05-24 09:21:32 -0400

    Add files via upload

---
+++ landingpage/include/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;600&family=Open+Sans:wght@600;200&family=Source+Code+Pro:wght@200;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/estilos.css">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="nav">
+            <div class="nav-left">
+                <a href="#">GoFinance</a>
+                <a href="#">Home</a>
+                <a href="#">Solution</a>
+                <a href="#">About us</a>
+            </div>
+            <div class="nav-right">
+                <a href="tel:+8494052758">Call us: 849-405-2758</a>
+            </div>
+        </div>
+    </header>
---
+++ landingpage/index.php
@@ -0,0 +1,137 @@
+
+<?php include('include/header.php') ?>
+
+<div class="primera-vista">
+
+    <div class="primeraVista-left">
+        <h1>Insurance for the <br> digita revolution</h1>
+        <div class="textoPequeno">
+            <div class="ralla">
+                <!-- SOLO DISENO -->
+            </div>
+            <p>Protect your communities and offer them exclusive insurance products</p>
+        </div>
+        <div class="btn-primeraVista">
+            <input type="button" value="LEARN MORE">
+            <input type="button" value="GET A QUOTE">
+        </div>
+    </div>
+
+    <div class="primeraVista-right">
+        <!-- SOLO DISENO -->
+    </div>
+
+</div>
+
+<div class="segunda-vista">
+
+    <div class="titulos-segundaVista">
+        <h1>OUR SERVICES</h1>
+        <h2>Best in-class solutions for</h2>
+    </div>
+
+    <div class="contenedor-imagenes">
+        <div class="img1-container">
+            <div class="img1"></div>
+            <h1>FINTECH</h1>
+        </div>
+        <div class="img2-container">
+            <div class="img2"></div>
+            <h1>DISRUPTIVE MOBILITY</h1>
+        </div>
+        <div class="img3-container">
+            <div class="img3"></div>
+            <h1>MARKETPLACE</h1>
+        </div>
+    </div>
+
+</div>
+
+<div class="tercera-vista">
+
+    <div class="titulos-terceraVista">
+        <h1>WHAT THEY SAY</h1>
+        <h2>You are in good hands</h2>
+    </div>
+
+    <div class="comentario">
+        <p class="texto-comentario">
+        Contrary to popular belief, Lorem Ipsum
+        is not simply random text. It has roots
+        in a piece of classical Latin literature from 45 BC
+        </p>
+        <div class="usuario">
+        <div class="img4"></div>
+            <div class="datos-comentario">
+                <h1>Luz Mariel</h1>
+                <p>0673 Goodwin Turnpike suite 262</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="cuarta-vista">
+
+    <div class="cuartaVista-left">
+        <div class="caja-azul">
+            <!-- DISENO -->
+        </div>
+        <div class="caja-amarilla">
+            <!-- DISENO -->
+        </div>
+    </div>
+    <div class="cuartaVista-right">
+        <h1>ABOUT US</h1>
+        <h2>We are GoFinance. <br>Please to meet you!</h2>
+        <h3>We rock the status quo ofinsurance</h3>
+        <div class="parrafo">
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Donec pulvinar odio vitae dolor finibus porttitor.
+        In pretium purus id vestibulum gravida.
+        Nunc egestas leo nec mauris suscipit, nec egestas lacus lobortis.
+        </p>
+        <p>
+        Etiam sed nibh at odio iaculis finibus at id sapien.
+        Fusce eu tortor et erat posuere pellentesque.
+        Duis facilisis justo eget nulla efficitur, vitae imperdiet massa convallis.
+        Sed convallis lorem ac ipsum pellentesque, vel vehicula urna eleifend.
+        </p>
+        </div>
+        <input type="button" value="JOIN NOW" class="btn-join">
+    </div>
+</div>
+
+<div class="footer">
+    <div class="footer-top">
+      <div class="gofinance">
+        <h1>GoFinance</h1>
+      </div>
+      <div class="services">
+        <h1>Services</h1>
+        <a href="#">Education</a>
+        <a href="#">Health</a>
+        <a href="#">Profession</a>
+      </div>
+      <div class="reach-us">
+        <h1>Reach Us</h1>
+        <a href="#">ask@gofinance.com</a>
+        <a href="#">gofinance.com/help</a>
+      </div>
+      <div class="account">
+        <h1>Account</h1>
+        <a href="#">Login</a>
+        <a href="#">Register</a>
+        <a href="#">Become a Platinum User</a>
+      </div>
+    </div>
+
+    <div class="footer-bottom">
+        <p>Also, follow us</p>
+        <img src="./imagenes/facebook.svg" alt="">
+        <img src="./imagenes/instagram.svg" alt="">
+        <img src="./imagenes/whatsapp.svg" alt="">
+    </div>
+</div>
+<?php include('include/footer.php') ?>
